fda671373fefa629beca6b6e3e03bdf3ea802834
Galiya <safaga71@mail.ru>
2023-01-31 22:11:43 +0300

    Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -5,48 +5,156 @@
 // [345, 897, 568, 234] -> 2
 
  
-    //Метод для создания массива размера size из случайных чисел из диапазона [start;end]
-            int[] GenerateRandomArray(int size, int start, int end)
-            {
-                //объявляем массив размера size( количество элементов = size)
-                // это выделена область памяти для переменных типа int (количество переменных = size)
-                int[] RandomArray = new int[size];
-                for (int i = 0; i < size; i++)
-                {
-                    //Генерируем чисело из диапазона [start;end] и записываем его в i-йэлемент массива
-                    RandomArray[i] = new Random().Next(start, end + 1);
-                }
-                //так как "int[] Gen" то вернуть должны что-то типа int[]. А это массив СОЗДАННЫЙ
-                return RandomArray;
-            }
+   
+ int[] GetRandomArr(int size, int start, int end)
+ {
+               
+    int[] RandomArr = new int[size];
+        for (int i = 0; i < size; i++)
+        {          
+         RandomArr[i] = new Random().Next(start, end + 1);
+        }
+         return RandomArr;
+}
 
-            //Метод для вывода элементов массива в консоль для вывода на экрана
-            //передаём "()" в метод ShowArray массив "int[] "
-            void ShowArray(int[] Array1)
-            {
-                for (int i = 0; i < Array1.Length; i++)
-                {
-                    Console.WriteLine(Convert.ToString(Array1[i]));
-                }
-            }
-            // Проверка количества чётных чисел (элементов) массива
-            int CountOfEven(int[] myArray2)
-            //передаём "()" в метод ShowArray массив "int[] ", возвращаем число "int Co..."
-            {
-                //Счётчик, куда сохраняем сколько раз попались чётные числа (при каждом новом нахождении +1)
-                int Count = 0;
-                for (int i = 0; i < myArray2.Length; i++)
-                    if (myArray2[i] % 2 == 0)
-                    {
-                        Count = Count + 1;
-                    }
-                return Count;
-            }
-
-            // ВЫЗОВ РАНЕЕ ОБЪЯВЛЕННЫХ МЕТОДОВ
-            int[] myArray = new int[5];
-            myArray = GenerateRandomArray(5, 100, 999);
-            ShowArray(myArray);
-            Console.WriteLine("количество четных чисел массива " + CountOfEven(myArray));
+ void ShowArray(int[] Array1)
+{
+     for (int i = 0; i < Array1.Length; i++)
+         {
+         Console.WriteLine(Convert.ToString(Array1[i]));
+        }
+}
            
+ int Count(int[] myArray2)
+     {
+     int count = 0;
+          for (int i = 0; i < myArray2.Length; i++)
+             if (myArray2[i] % 2 == 0)
+         {
+          count = count + 1;
+         }
+         return count;
+}
+
+     int[] myArray = new int[5];
+     myArray = GetRandomArr(5, 100, 999);
+     ShowArray(myArray);
+     Console.WriteLine("количество четных чисел массива " + Count(myArray));
+           
+  
+
+
+
+
+
+
+// Console.Clear();
+
+// int EvenCount(int[] array)
+// {
+//     int count = 0;
+//     for (int index = 0; index < array.Length; index++)
+//     {
+//         if (array[index] % 2 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i == 0) Console.Write($"[{array[i]}, ");
+//         else if (i == (array.Length - 1)) Console.Write($"{array[i]}]");
+//         else Console.Write($"{array[i]}, ");
+//     }
+// }
+
+// int[] GetArray(int size, int min, int max)
+// {
+//     int[] arr = new int[size];
+//     for (int index = 0; index < size; index++)
+//     {
+//         arr[index] = new Random().Next(min, max + 1);
+//     }
+//     return arr;
+// }
+
+// int GetSize(string text)
+// {
+//     Console.WriteLine(text);
+//     int size = int.Parse(Console.ReadLine());
+//     return size;
+// }
+
+// int size = GetSize("Введите размер массива:");
+// int[] array = GetArray(size, 100, 999);
+// PrintArray(array);
+// int count = EvenCount(array);
+// Console.Write($" -> {count}");
+
+
+
+
+// // +++Задача 38: Задайте массив вещественных чисел.
+// // Найдите разницу между максимальным и минимальным
+// // элементов массива.
+
+// // [3 7 22 2 78] -> 76
+// using System;
+
+// class Program {
+//   public static void Main (string[] args) {
+//     /*Задача 38: Задайте массив вещественных чисел. Найдите разницу 
+//             между максимальным и минимальным элементов массива.
+//             [3 7 22 2 78] -> 76
+//             */
+
+//             int Find_Min_Max_diff(int[] array)
+//             {
+//                 int result = 0;
+//                 int min = 0;
+//                 int max = 0;
+
+//                 for (int i = 0; i < array.Length; i++)
+//                 {
+//                     if (array[min] > array[i])
+//                         min = i;
+//                     else if (array[max] < array[i])
+//                         max = i;
+//                 }
+//                 Console.WriteLine("min= " + array[min] + "\tmax= " + array[max]);
+//                 if (array[min] > array[max])
+//                     result = array[min] - array[max];
+//                 else if (array[max] > array[min])
+//                     result = array[max] - array[min];
+//                 return result;
+//             }
+//              Console.Write("введите размер массива: ");
+//             int size = Convert.ToInt32(Console.ReadLine());
+//             int[] array = new int[size];
+//             Console.Write("введите минимальное значение: ");
+//             int Min = Convert.ToInt32(Console.ReadLine());
+//             Console.Write("введите максимальное значение: ");
+//             int Max = Convert.ToInt32(Console.ReadLine());
+//             int temp = 0;
+//             if (Min > Max)
+//             {
+//                 temp = Max;
+//                 Max = Min;
+//                 Min = temp;
+//             }
+//             Random rand = new Random();
+//             for (int i = 0; i < array.Length; i++)
+//                 array[i] = rand.Next(Min, Max);
+//             for (int i = 0; i < array.Length; i++)
+//                 Console.Write(array[i] + "\t");
+            
+
+//             Console.WriteLine("разница между минимальным и максимальным элементом массива=" + Find_Min_Max_diff(array));
+//   }
+// }
   
